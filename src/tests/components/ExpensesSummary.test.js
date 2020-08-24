bb031a7c1c7a2e972bfc1190379fddee332448a5
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-24 15:42:15 -0500

    renamed files

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ExpenseSummary from '../../components/ExpenseSummary';
+import ExpensesSummary from '../../components/ExpensesSummary';
 
 test('should connectly render expenseSummay with 1 expense', () => {    
-    const wrapper = shallow(<ExpenseSummary expensesCount={1} expensesTotal={10025}/>);
+    const wrapper = shallow(<ExpensesSummary expensesCount={1} expensesTotal={10025}/>);
     expect(wrapper).toMatchSnapshot();
   });
   
 
   test('should connectly render expenseSummay with 2 expenses', () => {   
-    const wrapper = shallow(<ExpenseSummary expensesCount={2} expensesTotal={8000000025}/>);
+    const wrapper = shallow(<ExpensesSummary expensesCount={2} expensesTotal={8000000025}/>);
     expect(wrapper).toMatchSnapshot();
   });
