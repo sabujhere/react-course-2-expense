cd8bb0a99c3f38504040667d40dc49e292b731b1
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-27 21:01:28 -0500

    creating test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 import moment from 'moment';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBINkmOD5xotJ0Tr-kwHWCZczEeXONplM4",
-    authDomain: "expensify-b120c.firebaseapp.com",
-    databaseURL: "https://expensify-b120c.firebaseio.com",
-    projectId: "expensify-b120c",
-    storageBucket: "expensify-b120c.appspot.com",
-    messagingSenderId: "238831287582",
-    appId: "1:238831287582:web:cdfdaca78446bdb48c5d0f",
-    measurementId: "G-0PG95BZWW8"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL:process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   };
 
   // Initialize Firebase
