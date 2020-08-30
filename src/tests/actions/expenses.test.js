504a3063ce98513d15c891a7a74e48a13ae7e6f6
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-30 18:02:51 -0500

    start edit test did not pass

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,15 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
+import { 
+  startAddExpense, 
+  addExpense, 
+  editExpense, 
+  removeExpense, 
+  setExpenses, 
+  startSetExpenses, 
+  startRemoveExpense,
+  startEditExpense 
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -31,6 +40,30 @@ test('should setup edit expense action object', () => {
     }
   });
 });
+
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const editId = expenses[1].id;
+  const updates =  {
+    amount: 21045
+  }
+  store.dispatch(startEditExpense(editId, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id:editId,
+      updates
+    });
+    return database.ref(`expenses/${editId}`).update(updates);
+  }).then((snapshot)=>{
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
+  });
+});
+
+
+
 
 test('should setup add expense action object with provided values', () => {
   const action = addExpense(expenses[2]);
@@ -138,4 +171,6 @@ test('should remove the expense by id from firebase', (done) => {
     done();
   });
 });
+
+
 
