eceb4a5c1310dd7390ed525a3f22b0d070fbd039
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-30 20:34:18 -0500

    implemeting logout

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Login from '../../components/LoginPage';
+import {LoginPage} from '../../components/LoginPage';
+
+let startLogin, wrapper;
+
+beforeEach(() => {
+  startLogin = jest.fn();
+  wrapper = shallow(
+    <LoginPage
+      startLogin={startLogin}
+    />
+  );
+});
 
 test('should render Login page correctly', () => {
-  const wrapper = shallow(<Login />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogin on button click', () => {
+  wrapper.find('button').simulate('click');  
+  expect(startLogin).toHaveBeenCalled();
 });
