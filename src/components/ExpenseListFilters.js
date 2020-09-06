22415e43b168599b58f9f418902630935732cff5
Sabuj_S&C <sabuj.saha@sandc.com>
2020-09-06 14:52:34 -0500

    syling list filters

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -26,12 +26,16 @@ export class ExpenseListFilters extends React.Component {
   };
   render() {
     return (
-      <div>
+      <div className="content-container">
+      <div className="input-group">
+        <div className="input-group__item">
         <input
           type="text"
           value={this.props.filters.text}
           onChange={this.onTextChange}
         />
+        </div>
+        <div className="input-group__item">
         <select
           value={this.props.filters.sortBy}
           onChange={this.onSortChange}
@@ -39,6 +43,8 @@ export class ExpenseListFilters extends React.Component {
           <option value="date">Date</option>
           <option value="amount">Amount</option>
         </select>
+        </div>
+        <div className="input-group__item">
         <DateRangePicker
           startDate={this.props.filters.startDate}
           endDate={this.props.filters.endDate}
@@ -49,6 +55,8 @@ export class ExpenseListFilters extends React.Component {
           numberOfMonths={1}
           isOutsideRange={() => false}
         />
+        </div>
+      </div>
       </div>
     );
   }
